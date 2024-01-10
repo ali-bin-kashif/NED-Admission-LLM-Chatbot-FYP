de62f2b4e13e716a7691e9b7e91315db2ec6374d
AL1 <alibinkashif007@gmail.com>
2024-01-10 14:43:32 +0500

    Comments added

--- ingest.py
+++ ingest.py
@@ -1,28 +1,39 @@
+# Importing libs and modules
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
-from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader, TextLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter, TextSplitter
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+# Setting path for input data files
 DATA_PATH = 'data/'
+
+# Path for vectorstore to store text embeddings made from the data
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
 # Create vector database
 def create_vector_db():
+    
+    # Load the PDF documents
     loader = DirectoryLoader(DATA_PATH,
                              glob='*.pdf',
                              loader_cls=PyPDFLoader)
-    
-    # loader = TextLoader('./data/ned_knowledge_base.txt')
 
     documents = loader.load()
+    
+    # Split the text into chunks
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=256,
                                                    chunk_overlap=32)
     texts = text_splitter.split_documents(documents)
 
+    # Using hugging face embeddings
     embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2',
                                        model_kwargs={'device': 'cpu'})
-
+    
+    # Converting all the chunks into text embeddings (Converting text into vectors)
+    # After text is converted into vectors, it can be used to many task like classifications etc.
     db = FAISS.from_documents(texts, embeddings)
+    
+    # Saving the embeddings in the vector store
     db.save_local(DB_FAISS_PATH)
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Importing libs and modules
 from langchain.prompts import PromptTemplate
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores.faiss import FAISS
@@ -5,13 +6,15 @@ from langchain_community.llms.ctransformers import CTransformers
 from langchain.chains import RetrievalQA
 from fastapi import FastAPI
 from pydantic import BaseModel
-# import chainlit as cl
 
+# Path of vectore database
 DB_FAISS_PATH = 'vectorstore/db_faiss'
 
-custom_prompt_template = """Use the following pieces of information to answer the user's question. Try to give the best answer
+
+# Prompt template
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
 If you don't know the answer in any way, just say this text
-"Sorry I can't help you on this query, Please see the guidelines and FAQs on the following link: https://www.neduet.edu.pk/admission, or if you have some special query you can contact the administrator".
+"Sorry I can't help you on this query, if you are facing problem you can contact the administrator Mr. Imran Shaikh on help desk. Please see the guidelines and FAQs on the following link: https://www.neduet.edu.pk/admission".
 Context: {context}
 Question: {question}
 
@@ -43,7 +46,7 @@ def load_llm():
     llm = CTransformers(
         model = "./llama-2-7b-chat.ggmlv3.q8_0.bin",
         model_type="llama",
-        max_new_tokens = 512,
+        max_new_tokens = 1024,
         temperature = 0.5
     )
     return llm
@@ -65,34 +68,11 @@ class validation(BaseModel):
 #Fast API
 app = FastAPI()
 
+# API endpoint to generate response
 @app.get("/llm_on_cpu/{query}")
-#output function
+
 async def final_result(query):
         qa_result = qa_bot()
         response = qa_result({'query': query})
         return response
- 
-
-# print(final_result('Background history of NED?'))
-#chainlit code
-# @cl.on_chat_start
-# async def start():
-#     chain = qa_bot()
-#     msg = cl.Message(content="Starting the bot...")
-#     await msg.send()
-#     msg.content = "Hi, Welcome to Medical Bot. What is your query?"
-#     await msg.update()
-
-#     cl.user_session.set("chain", chain)
-
-# @cl.on_message
-# async def main(message: cl.Message):
-#     chain = cl.user_session.get("chain") 
-#     cb = cl.AsyncLangchainCallbackHandler(
-#         stream_final_answer=True, answer_prefix_tokens=["FINAL", "ANSWER"]
-#     )
-#     cb.answer_reached = True
-#     res = await chain.acall(message.content, callbacks=[cb])
-#     answer = res["result"]
-#     await cl.Message(content=answer).send()
 
